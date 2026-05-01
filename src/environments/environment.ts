ef7587c36d7fff2dcc6f88c8714903456dade787
JihanDhaoui6 <jihandhaoui65@gmail.com>
2026-05-01 21:01:02 +0100

    Initial commit - DiaCare frontend authentication UI

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8081/api/admin' // ← backend Spring Boot
+  apiUrl: 'http://localhost:8081/api/admin', // ← backend Spring Boot
+   googleClientId: 'VOTRE_CLIENT_ID_GOOGLE_ICI'
 };
